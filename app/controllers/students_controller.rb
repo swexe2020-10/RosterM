29257684bab88216cb69f7ab43f9dc9c5b5fc017
EC2 Default User <ec2-user@ip-172-31-56-66.ec2.internal>
2020-12-21 06:54:15 +0000

    ログイン位置

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,13 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
 
   def new
     @student = Student.new
   end
+  
+  
 
   def create
     @student = Student.new(
